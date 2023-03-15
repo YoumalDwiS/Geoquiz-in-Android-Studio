389121945a6dc4db97ad88402664b65879eb1289
YoumalDwiS <youmaldwi@gmail.com>
2023-03-15 15:59:51 +0700

    Add prev & next button and dynamic question

--- app/src/main/java/id/ac/astra/polman/nim03202100079/geoquiz/MainActivity.java
+++ app/src/main/java/id/ac/astra/polman/nim03202100079/geoquiz/MainActivity.java
@@ -6,24 +6,64 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
 
+    private ImageButton mNextButton;
+
+    private ImageButton mPrevButton;
+
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+
+
+    };
+    private int mCurrentIndex = 0;
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getmTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].ismAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        }else{
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    public MainActivity() {
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this,
+//                        R.string.correct_toast,
+//                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
 
             }
         });
@@ -31,10 +71,41 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this,
+//                        R.string.incorrect_toast,
+//                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getmTextResId();
+        mQuestionTextView.setText(question);
+
+        mNextButton = findViewById(R.id.imageButton2);
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+//                int question = mQuestionBank[mCurrentIndex].getmTextResId();
+//                mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+        updateQuestion();
+
+        mPrevButton = findViewById(R.id.imageButton);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int index =  (mCurrentIndex-1 == -1) ? mQuestionBank.length-1 : mCurrentIndex-1;
+                mCurrentIndex = index;
+//                int question = mQuestionBank[mCurrentIndex].getmTextResId();
+//                mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+        updateQuestion();
     }
+
 }
---
+++ app/src/main/java/id/ac/astra/polman/nim03202100079/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package id.ac.astra.polman.nim03202100079.geoquiz;
+
+public class Question {
+    private int mTextResId;
+
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setmTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean ismAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setmAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    private boolean mAnswerTrue;
+
+
+}
